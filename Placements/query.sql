05d9676a81fcea610098aad7b6d2d3dc3f6ec15b
Pushkar Garg <43633663+PushkarGarg02@users.noreply.github.com>
2018-10-15 12:30:21 +0530

    Update query.sql

--- Placements/query.sql
+++ Placements/query.sql
@@ -1,6 +1,10 @@
-select studentID.id, p1.salary, studentID.friend_id, p2.salary 
-from friends studentID, packages p1, packages p2, friends friendID
+/*Write a query to output the names of those students whose best friends got offered a higher salary than them. Names must be ordered by the salary amount offered to the best friends. It is guaranteed that no two students got same salary offer.
+*/
+select s.name
+from friends studentID, packages p1, packages p2, friends friendID,students s
 where studentID.id = p1.id
 and friendID.id = p2.id
 and studentID.friend_id = friendID.id
-and p2.salary > p1.salary;
+and p2.salary > p1.salary
+and s.id = studentID.id
+order by p2.salary;
